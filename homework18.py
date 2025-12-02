eb34bfd8882c1493c5a4854b1fff84bf192ceb25
ssarvar09 <sarvarquryazov0@gmail.com>
2025-12-02 16:25:24 +0300

    Update homework18.py

--- homework18.py
+++ homework18.py
@@ -1,76 +1,89 @@
-# def user_data(first_name, last_name, age):
-#     return {
-#         'ism': first_name,
-#         'familiya': last_name,
-#         'yosh': age,
-#     }
-# ism = input('ismingniz kiriting:')
-# familiya =input('familiyanginz kiriting:')
-# yosh = input('yoshingizni kiriting:')
-# i = user_data(ism,familiya,yosh)
-#
-# print('Ism:',i['ism'])
-# print('Familiya:',i['familiya'])
-# print('Yosh:',i['yosh'])
+class Fan:
+    def __init__(self, nom):
+        self.nom = nom
 
 
-# def find_max(a,b,c):
-#     max_value = max(a,b,c)
-#     k=[]
-#     if a == max_value:
-#        k.append('A')
-#     if b == max_value:
-#        k.append('B')
-#     if c == max_value:
-#        k.append("C")
-#
-#     if len(k) ==1:
-#         print(f"Eng katta son - {k[0]} {max_value}")
-#     elif len(k) ==2:
-#         print(f"Eng katta son - {k[0]} va {k[1]} {max_value}")
-#     else:
-#         print(f"eng katta son - A  va B va C {max_value}")
-#
-# a = int(input(" A sonni kiriting:"))
-# b = int(input(" B sonni kiriting:"))
-# c = int(input(" C sonni kiriting:"))
-# find_max(a,b,c)
+class Talaba:
+    def __init__(self, ism, yosh):
+        self.ism = ism
+        self.yosh = yosh
+        self.fanlar = []
 
 
-# ### 3
-# def find_letter_count(word, letter):
-#     c = word.count(letter)
-#     print(f'"{word}" soʻzida "{letter}" harfi {c} marta qatnashgan.')
-#
-# # Foydalanuvchidan soʻz va harfni olish
-# word = input("Soʻz kiriting: ")
-# letter = input("Qaysi harfni sanaymiz: ")
-# find_letter_count(word, letter)
-#
+    def fanga_yozil(self, fan):
+        self.fanlar.append(fan)
+
+    def remove_fan(self, fan):
+        if fan in self.fanlar:
+            self.fanlar.remove(fan)
+        else:
+            print("Siz bu fanga yozilmagansiz")
+
+    def get_info(self):
+        fanlar = [fan.nom for fan in self.fanlar]
+        return f"Talaba: {self.ism}, Yosh: {self.yosh}, Fanlar: {fanlar}"
 
 
-
-### 4
-# def list_sum(myList):
-#     t = sum(myList)
-#     print(f"Listning elementlar yig'indisi = {t}")
-# list_sum([5, 7, 10, 10])
+class Professor:
+    def __init__(self, ism, fan):
+        self.ism = ism
+        self.fan = fan
 
 
+    def get_info(self):
+        return f"Professor: {self.ism}, Fan: {self.fan}"
 
-### 5
-# def daraja(a, b):
-#     s = a ** b
-#     print(f"{a} ning {b}-darajasi = {s}")
-# daraja(5, 2)
-#
-#
-# ### 6
-#
-# def daraja4(a, b, c, d):
-#     print(f"{a} ning {b}-darajasi = {a ** b}")
-#     print(f"{a} ning {c}-darajasi = {a ** c}")
-#     print(f"{a} ning {d}-darajasi = {a ** d}")
-# araja4(2, 3, 4, 5)
 
-d
+class Foydalanuvchi:
+    def __init__(self, ism, login):
+        self.ism = ism
+        self.login = login
+
+
+    def get_info(self):
+        return f"Foydalanuvchi: {self.ism}, Login: {self.login}"
+
+
+class Mijoz:
+    def __init__(self, ism, balans):
+        self.ism = ism
+        self.balans = balans
+
+
+    def get_info(self):
+        return f"Mijoz: {self.ism}, Balans: {self.balans}"
+
+
+class Admin(Foydalanuvchi):
+    def ban_user(self):
+        print("Foydalanuvchi bloklandi")
+
+
+    def get_info(self):
+        return f"Admin: {self.ism}, Login: {self.login}"
+
+
+math = Fan("Matematika")
+eng = Fan("Ingliz tili")
+
+
+talaba = Talaba("Ali", 17)
+talaba.fanga_yozil(math)
+talaba.fanga_yozil(eng)
+talaba.remove_fan(math)
+
+
+prof = Professor("Sarvar", "Ingliz")
+user = Foydalanuvchi("Sarvar", "sarvar0990")
+mijoz = Mijoz("Ali", 20000)
+admin = Admin("Amir", "amirr08")
+
+
+admin.ban_user()
+
+
+print(talaba.get_info())
+print(prof.get_info())
+print(user.get_info())
+print(mijoz.get_info())
+print(admin.get_info())
